f11b47481f074ff6ad664708b7cd8b8f7972b1d6
lachhab-anas <anaslachhab19@gmail.com>
2021-01-12 15:56:36 +0100

    make it better

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var qr_btn = document.querySelector("#qr-btn")
 var qr_img = document.querySelector("#qr-img")
 var loading = document.querySelector("#loading")
+var a = document.querySelector("#a")
+
+var url;
 
 qr_btn.addEventListener("click", () => {
     var input = document.querySelector("#input").value
@@ -14,8 +17,11 @@ qr_btn.addEventListener("click", () => {
     // generate image
     
     
-        var url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`
+        url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`
         qr_img.src = url
+        qr_img.alt = input
+        a.href = url+".png"
     }
    
 })
+
